d17391d25b6c5fa78c26ed91c4c8e7be764a4a0a
EpicGhani <al_ghani12@yahoo.com>
2019-01-28 17:36:28 +0800

    Initial commit
    Project Magellan Master

---
+++ Magellan/Assets/_Stuff/Scripts/BoatAnimMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatAnimMovement : MonoBehaviour 
+{
+	public Transform waypoint;
+	public float speed;
+
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if(Vector3.Distance(transform.position,waypoint.position) >= 0.2f)
+		{
+			Vector3 dir = waypoint.position - transform.position;
+			transform.LookAt(new Vector3(waypoint.position.x,transform.position.y,waypoint.position.z));
+			transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+		}
+	}
+}
