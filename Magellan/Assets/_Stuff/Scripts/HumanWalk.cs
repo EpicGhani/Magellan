9ce95ce6e8ffba1380f2bd51303fb746462b87c6
Harold Cha <chaharold03@gmail.com>
2019-01-29 20:15:46 +0800

    Script Edited

--- Magellan/Assets/_Stuff/Scripts/HumanWalk.cs
+++ Magellan/Assets/_Stuff/Scripts/HumanWalk.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HumanWalk : MonoBehaviour {
+[RequireComponent(typeof(CharacterController))]
+public class HumanWalk : MonoBehaviour
+{
+	public float speed;
+	public float rotDamping;
+	public bool randomize;
+	public Transform[] waypoints;
+
+	CharacterController cc;
+	Transform target;
 
 	// Use this for initialization
 	void Start () {
-		
+		cc = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+
+
+
+		Vector3 tar = new Vector3 (target.position.x, transform.position.y, target.position.z);
+
+		var rot = Quaternion.LookRotation(tar - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime * rotDamping);
 	}
 }
